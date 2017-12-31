fa3a1a302209cdeeb647a63c7ae9900d97502522
Brandon Lucas Zhu <Brandonlucaszhu@gmail.com>
2017-12-31 00:01:30 -0500

    Preparing search fpr valid menus

--- MenuValidation/backend_challenge.py
+++ MenuValidation/backend_challenge.py
@@ -21,15 +21,18 @@ def getAllPages(link):
 
 def getAllData(root, storeValues, link):
     
+    storedValues = []
     #Go through all pages
     for num_pages in range(1,getAllPages(link)):
         #Iterate through all of the pages 
         totalresponse = requests.get("https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=1&page="+str(num_pages))
         
+        #Obtain entire json and store all root node information into one array
         childvalues = json.loads(totalresponse.text)
-        print (childvalues[root])
+        storedValues += childvalues[root]
         
-    return storeValues
+        
+    return storedValues
 
 if __name__ == "__main__":
      
@@ -40,9 +43,26 @@ if __name__ == "__main__":
     
     #Get all Data for Menu's 
     entireMenu = getAllData("menus", storeMenus, linkpages)
-        
-       
     
+    #print (entireMenu)
+    
+    root_id = ""
+    children = []
+        
+    for i in range(len(entireMenu)):
+        for j in range(len(entireMenu[i]["child_ids"])):
+            if "parent_id" in entireMenu[i]:
+                saveChildId = entireMenu[i]["child_ids"][j] 
+                #print (saveChildId)
+            else:
+                #store root_id and begin search through child node menus
+                root_id = entireMenu[i]["id"]
+                saveChildId = entireMenu[i]["child_ids"][j] 
+                print (saveChildId)    
+            #for j in range(len(saveChildId)):    
+            #    print (entireMenu[saveChildId[j]])
+    
+        
     
     
     
