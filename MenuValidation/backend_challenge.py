e10dc0c559e31ae437d2789d883056eed94106b6
EZZHUBR <brandon.zhu@ericsson.com>
2018-01-03 16:54:48 -0500

    testing other path

--- MenuValidation/backend_challenge.py
+++ MenuValidation/backend_challenge.py
@@ -37,7 +37,7 @@ def getAllData(root, storeValues, link):
 def isValidMenu(rootId, checkChildIds, entireMenuData):
     nextId = ""
     queue = [rootId]
-    explored = {}
+    explored = []
     pathChildIdLen = 0;
 
     while queue:
@@ -45,41 +45,53 @@ def isValidMenu(rootId, checkChildIds, entireMenuData):
         node = queue.pop(0)
         
         if node not in explored:
-            explored[node] = entireMenuData[int(node[len(node)-1])-1]['child_ids']
+            explored.append(node)
             
             #if pathChildIdLen == 0:
-            child_ids = entireMenuData[int(node[len(node)-1])-1]['child_ids'] #Get last 
+            child_ids = entireMenuData[int(node)-1]['child_ids'] #Get last 
             #else:
                 
                #child_ids = entireMenuData[int(node[0])-1]['child_ids']
 
-            pathChildIdLen = len(child_ids)   #Need this to check and iterate all the path options in child_ids as it re-loops
+            #pathChildIdLen = len(child_ids)   #Need this to check and iterate all the path options in child_ids as it re-loops
             
-            if len(child_ids) > 0:
-                lastParent = explored[len(explored)-1] #Add last index from explored if there is multiple paths
-                explored.pop(len(explored)-1)#Pop last index
-                for child_id in child_ids:
-                    morePaths = []
-                    morePaths.append(lastParent) 
-                    morePaths.append(child_id)
-                    queue.append(morePaths)
+            #if len(child_ids) > 0:
+                #lastParent = explored[len(explored)-1] #Add last index from explored if there is multiple paths
+                #explored.pop(len(explored)-1)#Pop last index
+            for child_id in child_ids:
+                    #morePaths = []
+                    #morePaths.append(lastParent) 
+                    #morePaths.append(child_id)
+                queue.append(child_id)
 
             #else:
                 #queue.append(child_id)        
 
-    #print (explored)    
+    print (explored)    
     return explored                
 
-def isAValidMenu(rootId, checkChildIds, entireMenuData):
-    save = []
-    #dfs_paths
-    stack = [(int(rootId), [int(rootId)])]
-    while stack:
-        (vertex, path) = stack.pop()
-        for next in entireMenuData[vertex]['child_ids']:
-            print stack
-            stack.append((next,path + [next]))
-    return 0
+def isAValidMenu(rootId, end, entireMenuData):
+    
+    queue = []
+    queue.append([rootId])
+    #stack = [(int(rootId), [int(rootId)])]
+    while queue:
+        path = queue.pop(0)
+        node = path
+        if node == end:
+            return path
+        for adjacent in entireMenuData[int(node)-1]['child_ids']:
+            new_path = list(path)
+            new_path.append(adjacent)
+            queue.append(new_path)
+            
+        #(vertex, path) = stack.pop(0)
+        #for next in entireMenuData[vertex-1]['child_ids']:
+        #    if next == "":
+        #        yield path + [next_node]
+        #    else:
+        #        stack.append((next,path + [next]))
+   
     
 
 if __name__ == "__main__":
@@ -106,8 +118,11 @@ if __name__ == "__main__":
 
         	for j in range(len(entireMenu[i]["child_ids"])):
                     saveChildId = entireMenu[i]["child_ids"][j]
-                    print (isAValidMenu("2", saveChildId, entireMenu))
-
+                    #isValidMenu("2", saveChildId, entireMenu)
+                    print isAValidMenu(root_id, "" ,entireMenu)
+                    #for val in isAValidMenu( root_id , "" , entireMenu ):
+                        #print (val)
+                    #[ print (x) for x in isAValidMenu( root_id , "" , entireMenu ) ]
 
                 #print (saveChildId)
             #else:
